086c5d2267656848880db2e3b7bf94055fc10873
Rueilian <rueilianting1639@gmail.com>
2026-01-09 00:11:09 +0800

    Convert API to CommonJS for Vercel compatibility

--- api/data.js
+++ api/data.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -33,7 +33,7 @@ const dataSchema = new mongoose.Schema({
 
 const SensorData = mongoose.models.SensorData || mongoose.model('SensorData', dataSchema);
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -66,4 +66,4 @@ export default async function handler(req, res) {
     console.error('Error:', error);
     return res.status(500).json({ error: error.message });
   }
-}
+};
