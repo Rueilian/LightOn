07a853fbc6f9ce708aaed24b4f82a583bf09400d
Rueilian <rueilianting1639@gmail.com>
2026-01-09 00:01:25 +0800

    Add CORS headers to API endpoints

--- api/history.js
+++ api/history.js
@@ -44,6 +44,17 @@ const dataSchema = new mongoose.Schema({
 const SensorData = mongoose.models.SensorData || mongoose.model('SensorData', dataSchema);
 
 export default async function handler(req, res) {
+  // Enable CORS
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method === 'GET') {
     try {
       await connectDB();
@@ -57,3 +68,4 @@ export default async function handler(req, res) {
 
   return res.status(405).json({ error: 'Method not allowed' });
 }
+
