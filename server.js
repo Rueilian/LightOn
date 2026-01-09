0330d94d4f904929f2a13d48447a1fb7583c7a2c
Rueilian <rueilianting1639@gmail.com>
2026-01-09 20:09:59 +0800

    Remove outdated MongoDB connection options

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('✓ Connected to MongoDB'))
   .catch(err => console.error('✗ MongoDB connection error:', err.message));
 
